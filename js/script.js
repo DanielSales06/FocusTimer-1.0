47555d987b3de2e459686867af7b85fe22c5457e
daniel <daniel.sales0610@outlook.com>
2023-04-24 17:38:34 -0300

    ..

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-const play = document.querySelector(".play");
-const pause = document.querySelector(".pause");
-const stop = document.querySelector(".stop");
-const set = document.querySelector(".set");
+const buttonPlay = document.querySelector(".play");
+const buttonPause = document.querySelector(".pause");
+const buttonStop = document.querySelector(".stop");
+const buttonSet = document.querySelector(".set");
+const buttonSoundOn = document.querySelector(".sound-on");
+const buttonSoundOff = document.querySelector(".sound-off");
 
-play.onclick = handleClick;
-pause.onclick = handleClick;
+buttonPlay.addEventListener('click', function() {
+  buttonPlay.classList.add('hide')
+  buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
+})
 
-function handleClick() {
-  play.classList.toggle("hide")
-  pause.classList.toggle("hide")
-}
+buttonPause.addEventListener('click', function() {
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+})
+
+buttonSoundOff.addEventListener('click', function() {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function() {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
+})
